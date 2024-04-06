71c0df05056611efcd979bc07d9ffde09aef3749
Lucas Rosa <lrosa@keeta.com>
2024-04-05 23:19:08 -0300

    build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export', // Export the application as a static HTML-only site
+  // output: 'standalone', // Export the application with a Node.js server lightweight server
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
