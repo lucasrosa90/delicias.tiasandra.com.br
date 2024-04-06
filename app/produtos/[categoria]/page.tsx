ce5cf9b0e2afc96bdad247a8f546df16c1154afc
Lucas Rosa <lrosa@keeta.com>
2024-04-05 22:14:13 -0300

    build

--- app/produtos/[categoria]/page.tsx
+++ app/produtos/[categoria]/page.tsx
@@ -1,5 +1,11 @@
+import slugify from "@/core/utils/slugify";
 import ListProducts from "@/products/components/organisms/ListProducts";
+import listCategories from "@/products/services/listCategories";
 import listProducts from "@/products/services/listProducts";
+
+export async function generateStaticParams() {
+  return listCategories().map(categoria => ({ categoria: slugify(categoria) }))
+}
 
 export default function ListarProdutosPorCategoria({ params: { categoria }}: { params: { categoria: string; }}) {
   return (
