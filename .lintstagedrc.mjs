180fe7a2d2f51b3d9e8f322b6202be9393b9c3cf
Lucas Rosa <lrosa@keeta.com>
2024-05-13 23:52:20 -0300

    lint

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,6 +1,6 @@
 const lintStagedConfig = {
   '**/*.{ts,tsx}': () => 'tsc --project tsconfig.json --noEmit --pretty',
-  '**/*.{js,ts,jsx,tsx}': ['eslint --fix', 'prettier --write -u'],
+  '**/*.{js,ts,jsx,tsx}': ['eslint', 'prettier --write -u'],
   '!**/*.{js,ts,jsx,tsx}': 'prettier --write -u',
 }
 
